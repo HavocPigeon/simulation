a34bf847126d03cf07d520717ce9bf2066465eaa
HavocPigeon <speeas@gmail.com>
2018-07-06 10:36:58 -0700

    initial commit

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const massive = require('massive');
+require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.json());
+
+//*** to check if connection is working: 
+console.log('connection string', process.env.CONNECTION_STRING);
+
+const port = 4000;
+app.listen(port, () => console.log(`server is listening on port ${port}`));
+
+//***set db so it can be accessed later
+massive(process.env.CONNECTION_STRING).then(database => {
+  app.set('db', database);
+}).catch(error => {
+  console.log('-------------- error', error);
+});
